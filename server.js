df5426f41da538f995673273e6c173f07ac899ce
vanesa <vanesa@vanesas-MacBook-Pro.local>
2021-06-01 16:40:13 -0400

    completed readme and html

---
+++ server.js
@@ -0,0 +1,14 @@
+//requirement of the port server
+const 
+const PORT= 3000
+
+
+
+pp.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
+
+// Start the server on the port
+app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
