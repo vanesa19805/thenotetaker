be1f368fedcb9740242ffae48d74e19805db0433
vanesa <vanesa@vanesas-MacBook-Pro.local>
2021-06-01 17:11:36 -0400

    completed the html routes

---
+++ routes/htmlRoutes.js
@@ -0,0 +1,14 @@
+//this will the routes from the 2 html pages
+// start off with the requirements
+const path = require('path');
+const router= require (express).Router();
+
+// refrence to the notes.html
+router.get('/notes.html',(res,req)=>{
+    res.sendFile(path.join(__dirname,'/notes.html'));
+});
+
+//refrence to the index.html
+router.get('index.html',(res,req)=>{
+    res.sendFile(path.join(__dirname,'/index.js'));
+});
