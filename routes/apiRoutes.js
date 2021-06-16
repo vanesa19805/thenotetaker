24e2f2cee2319422883351aa4fee1cb71860a397
vanesa19805 <gvane1919@gmail.com>
2021-06-16 17:19:24 -0400

    added holds.js

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,16 +1,11 @@
 //need to require express Route
 const router = require('express').Router();
-
-const store = require ("..db/store");
+const store = require ("..db/holds.js");
 //we will also use the npm UUID to get a unique id
 
 
-
-
-
-//GET
 //`GET /api/notes` should read the `db.json` file and return all saved notes as JSON.
-router.get('./notes', (req,res)=>{
+router.get('./holds', (req,res)=> {
     store
     .getNote("")
     .then((notes)=>{
@@ -21,7 +16,7 @@ router.get('./notes', (req,res)=>{
 
 //POST
 //`POST /api/notes` should receive a new note to save on the request body, add it to the `db.json` file, and then return the new note to the client. You'll need to find a way to give each note a unique id when it's saved (look into `npm` packages that could do this for you).
-router.post('/notes',(req,res)=>{
+router.post('/holds',(req,res) =>{
     store
     .addNote((req.body))
     .then((note) => res.json(note))
